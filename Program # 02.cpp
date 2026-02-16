76907643de92f7dfc9c04ff8c79177738c4d78e3
Asadullah <asadullahyounas1437@gmail.com>
2026-02-16 16:38:20 +0500

    Add files via upload

---
+++ Program # 02.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+ int main ()
+ {
+ 	int number;
+ 	 cout<<"Enter the Number "<<endl;
+ 	 cin>>number;
+ 	 if ( number >0)
+ 	 {
+ 	 	cout<<" The number you entered is postive:\t"<<number<<endl;
+ 	 	
+	  }
+	  else if( number < 0)
+	  {
+	  	cout<<"The number you entered is negative \t"<<number<<endl;
+	  	
+	  }
+	  
+	  else
+	  {
+	  	cout<< " The number you entered is  \t"<<number<<endl;
+	  }
+	  return 0;
+	  
+	  
+ }
