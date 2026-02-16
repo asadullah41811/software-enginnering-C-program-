ea5f375189757fcbf3c915726383f1c646594e62
Asadullah <asadullahyounas1437@gmail.com>
2026-02-16 16:36:49 +0500

    Add files via upload

---
+++ Program # 01.cpp
@@ -0,0 +1,25 @@
+//write the program to check the number is even or odd
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int num;
+	cout<<"  Please enter the Number::"<<endl;
+	cin>>num;
+	
+	if(num % 2 == 0)
+		
+	cout<<"The number you entered is  even:: \n"<<  num;
+	else
+	
+		cout<<" The number you entered  is odd : \n"<< num;
+	
+	
+		
+		
+	
+	return 0;
+	
+	
+};
