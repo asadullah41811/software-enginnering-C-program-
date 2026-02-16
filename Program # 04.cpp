dde996e404ceb9fa68dbc318e296d84489b57e65
Asadullah <asadullahyounas1437@gmail.com>
2026-02-16 16:39:00 +0500

    Add files via upload

---
+++ Program # 04.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+ 	 int year;
+ 	 cout<<"Enter the  year::"<<endl;
+ 	 cin>>year;
+ 	 if ( year%4==0 || year % 400==0)
+ 	 {
+ 	 	cout<<"Year is the leap year:"<<endl;
+ 	 	
+	  }
+	
+	   else 
+	   {
+	   	cout<<" Year is not the leap year"<<endl;
+	   }
+ 	 return 0;
+ }
