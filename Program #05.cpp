fdf67b339ab799fbe97c40c611088f961cfb3395
Asadullah <asadullahyounas1437@gmail.com>
2026-02-17 15:03:12 +0500

    Add files via upload

---
+++ Program #05.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	 int num;
+	  cout<<"Enter the number ::"<<endl;
+	  cin>>num;
+	  if 
+	  ( num% 5== 0 && num % 11==0)
+	  {
+	   cout<<"The number is divisible by 5 or 11::" <<num<<endl;
+	  }
+	  else
+	  {
+	  	 cout<<"The number is   not divisible by 5 or 11 " << num<<endl;
+	  	 
+	  }
+	  return 0;
+}
