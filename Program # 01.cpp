968dd32450f7337f40c36539329caf5c8dd3c7fd
Asadullah <asadullahyounas1437@gmail.com>
2026-02-18 09:39:56 +0500

    Update Program # 01.cpp

--- Program # 01.cpp
+++ Program # 01.cpp
@@ -1,4 +1,4 @@
-//write the program to check the number is even or odd
+//write the program to check the number is even or odd//
 #include<iostream>
 using namespace std;
 int main()
@@ -23,3 +23,4 @@ int main()
 	
 	
 };
+
